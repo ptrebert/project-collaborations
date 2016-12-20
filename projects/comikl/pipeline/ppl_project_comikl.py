5ad6ae152ad89c4643a8c7b497ccf4e5ea786707
Peter Ebert <pebert@mpi-inf.mpg.de>
2016-12-20 16:28:50 +0100

    ENH: finished preliminary pipeline up to task of calling nuc positions

--- projects/comikl/pipeline/ppl_project_comikl.py
+++ projects/comikl/pipeline/ppl_project_comikl.py
@@ -7,6 +7,7 @@ import re as re
 import csv as csv
 import json as js
 import statistics as stat
+import collections as col
 
 from ruffus import *
 
@@ -62,7 +63,7 @@ def collect_fitpars_results(inputfiles, outputfile, regexp):
     :return:
     """
     params = dict()
-    sigmas = []
+    sigmas = col.defaultdict(list)
     matcher = re.compile(regexp)
     for inpf in inputfiles:
         fp, fn = os.path.split(inpf)
@@ -72,13 +73,68 @@ def collect_fitpars_results(inputfiles, outputfile, regexp):
         if lib == 'Input':
             continue
         sigma, flen = extract_sigma_flen(inpf)
-        sigmas.append(sigma)
+        sigmas[sample].append(sigma)
         params[sample + '_' + lib] = flen
-    sigma = int(stat.median(sigmas))
-    params['sigma'] = sigma
+    params['samples'] = list(sigmas.keys())
+    for k, sig in sigmas.items():
+        s = int(stat.median(sig))
+        params[k + '_sigma'] = s
     with open(outputfile, 'w') as outf:
-        js.dump(params, outf)
+        js.dump(params, outf, indent=1, sort_keys=True)
     return outputfile
+
+
+def filter_bamfiles(bams, sample):
+    """
+    :param bams:
+    :param sample:
+    :return:
+    """
+    red = filter(lambda x: os.path.basename(x).startswith(sample), bams)
+    histones = []
+    control = []
+    for fp in red:
+        if '_Input_' in fp:
+            control.append(fp)
+        else:
+            histones.append(fp)
+    assert len(histones) == 6, 'Missing histone mark for sample {}: {}'.format(sample, histones)
+    if len(control) > 1:
+        control = [x for x in control if 'N.mrg.' in x]
+    assert len(control) == 1, 'Could not identify control BAM: {}'.format(control)
+    return histones, control
+
+
+def build_callnucs_arguments(bamfiles, paramfile, outdir, cmd, jobcall):
+    """
+    :param bamfiles:
+    :param paramfile:
+    :param outdir:
+    :param cmd:
+    :param jobcall:
+    :return:
+    """
+    params = js.load(open(paramfile, 'r'))
+    samples = params['samples']
+    arglist = []
+    for smp in samples:
+        hist, ctrl = filter_bamfiles(bamfiles, smp)
+        sigma = params[smp + '_sigma']
+        argd = {'sigma': sigma, 'prefix': smp, 'outdir': outdir, 'control': ctrl[0]}
+        bamopts = ''
+        for h in hist:
+            fn = os.path.basename(h)
+            smplib = fn.split('.')[0].rsplit('_', 2)[0]
+            flen = params[smplib]
+            bamopts += '-in {} -fLen {} '.format(h, flen)
+        argd['bamfiles'] = bamopts
+        tmp = cmd.format(**argd)
+        outfile = os.path.join(outdir, smp + '.bed')
+        hist.extend(ctrl)
+        arglist.append([hist, outfile, tmp, jobcall])
+    if bamfiles:
+        assert arglist, 'No arguments created'
+    return arglist
 
 
 def build_pipeline(args, config, sci_obj):
@@ -166,10 +222,25 @@ def build_pipeline(args, config, sci_obj):
                            output=os.path.join(dir_nucfit, 'sigma_fraglen_params.json'),
                            extras=['(?P<SAMPLE>\w+)_(?P<LIB>[A-Za-z0-9]+)_(S|F)_(1|2|N)\.dat'])
 
+    sci_obj.set_config_env(dict(config.items('MemJobConfig')), dict(config.items('EnvConfig')))
+    if args.gridmode:
+        jobcall = sci_obj.ruffus_gridjob()
+    else:
+        jobcall = sci_obj.ruffus_localjob()
+
+    cmd = config.get('Pipeline', 'nuccall')
+    dir_nuccall = os.path.join(workdir, 'nuccall')
+    nuccall = pipe.files(sci_obj.get_jobf('ins_out'),
+                         build_callnucs_arguments(collect_full_paths(dir_filtered, '*.bam'),
+                                                  os.path.join(dir_nucfit, 'sigma_fraglen_params.json'),
+                                                  dir_nuccall,
+                                                  cmd, jobcall),
+                         name='nuccall').mkdir(dir_nuccall)
+
     run_comikl = pipe.merge(task_func=touch_checkfile,
                             name='run_comikl',
                             input=output_from(rawdata_init, bamfilter, bammerge,
-                                              bamidx, nucfit, nucparams),
+                                              bamidx, nucfit, nucparams, nuccall),
                             output=os.path.join(workdir, 'run_project_comikl.chk'))
 
     return pipe
